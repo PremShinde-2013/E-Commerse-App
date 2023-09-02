11773c44723438d505bdc20ca64f3e2589c48d53
Prem Prakash Shinde <shindeprem102@gmail.com>
2023-09-02 23:26:43 +0530

    completed navbar and banner

--- client/src/components/Home/Banner/Banner.jsx
+++ client/src/components/Home/Banner/Banner.jsx
@@ -1,6 +1,24 @@
 import "./Banner.scss";
+import BannerImg from "../../../assets/banner-img.png";
 const Banner = () => {
-    return <div>Banner</div>;
+  return (
+    <div className='hero-banner'>
+      <div className='content'>
+        <div className='text-content'>
+          <h1>SALES</h1>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laudantium
+            atque veniam id ratione libero soluta.
+          </p>
+          <div className='ctas'>
+            <div className='banner-cta'>Read More</div>
+            <div className='banner-cta v2'>Shop Now</div>
+          </div>
+        </div>
+        <img className='banner-img' src={BannerImg} alt='' />
+      </div>
+    </div>
+  );
 };
 
 export default Banner;
